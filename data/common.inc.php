a0e31d2dd5fe7995db330ca1cb9da4c48177c47f
v-jiapji@HW-JIAPJI-02 <soullessl@qq.com>
2016-10-13 16:58:34 +0800

    vs code update localhost database

--- data/common.inc.php
+++ data/common.inc.php
@@ -1,9 +1,10 @@
 <?php
 //数据库连接信息
+//$cfg_dbhost = '148.72.232.123';
 $cfg_dbhost = 'localhost';
-$cfg_dbname = 'Dede';
-$cfg_dbuser = 'root';
-$cfg_dbpwd = '1357924680';
+$cfg_dbname = 'bettertreedede';
+$cfg_dbuser = 'bettertreesh';
+$cfg_dbpwd = 'Zxc123!@#';
 $cfg_dbprefix = 'dede_';
 $cfg_db_language = 'utf8';
 
